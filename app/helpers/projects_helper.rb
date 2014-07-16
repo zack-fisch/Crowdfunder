e35150f9dc07452849fee891c45ba281559848cf
Zack Fisch <zack@zfisch.com>
2014-07-16 15:07:00 -0400

    Adding dynamic display to index view, adding helper method 'wording' to view to determine how to display remaining time for funding of project

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+	def wording(project)
+		if project.pledging_ends_on > Time.now
+			content_tag(:strong, time_ago_in_words(project.pledging_ends_on) + " from now")
+		else 
+			content_tag(:strong, 'Project Funding Finished')
+		end
+	end
 end
