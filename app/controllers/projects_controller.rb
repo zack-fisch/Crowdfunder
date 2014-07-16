98e1692e220eb50d4f68470352c45958e9b99ea4
Zack Fisch <zack@zfisch.com>
2014-07-16 15:42:48 -0400

    Adding rescue for improper id search, adding ability to view indiviudal projects via params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,11 @@ class ProjectsController < ApplicationController
 	def index
 		@projects = Project.all
 	end
+
+	def show
+		@project = Project.find(params[:id])
+
+	rescue ActiveRecord::RecordNotFound
+		redirect_to :action => 'index'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Crowdfund::Application.routes.draw do
  get 'projects' => 'projects#index'
+ get 'projects/:id' => 'projects#show'
 end
